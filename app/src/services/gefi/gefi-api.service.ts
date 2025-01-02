b8b22b49da7ce8706522b53fc0fadef01756475c
jfuregatti <jfuregatti@gmail.com>
2025-01-02 17:10:02 -0300

    change the gefi api service to observe the response

--- app/src/services/gefi/gefi-api.service.ts
+++ app/src/services/gefi/gefi-api.service.ts
@@ -14,23 +14,23 @@ export class GefiApiService {
   }
 
   post(path: string, data: Object): Observable<any> {
-    return this.http.post(this.buildUrl(path), data);
+    return this.http.post(this.buildUrl(path), data, { observe: 'response' });
   }
 
   update(path: string, data: Object): Observable<any> {
-    return this.http.put(this.buildUrl(path), data);
+    return this.http.put(this.buildUrl(path), data, { observe: 'response' });
   }
 
   patch(path: string, data: Object): Observable<any> {
-    return this.http.patch(this.buildUrl(path), data);
+    return this.http.patch(this.buildUrl(path), data, { observe: 'response' });
   }
 
   delete(path: string): Observable<any> {
-    return this.http.delete(this.buildUrl(path));
+    return this.http.delete(this.buildUrl(path), { observe: 'response' });
   }
 
   private buildUrl(path: string): string {
-    if (!path.endsWith('/')) {
+    if (!path.endsWith('/') && !path.includes("/?")) {
       path = path + "/";
     }
     return `${GefiApi.url}/${path}`;
